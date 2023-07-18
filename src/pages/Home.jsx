ee04b7cda4d172eb1b660b455fc286f845203bb4
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-07-18 22:01:56 +0100

    More changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,37 @@
-import {
-    Swiper,
-    SwiperSlide
-} from 'swiper/react'
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import SwiperCore, {
-    EffectFade,
-    Mousewheel,
-    Pagination
-} from 'swiper'
+import SwiperCore, { EffectFade, Mousewheel, Pagination } from "swiper";
 
 import {
-    Welcome,
-    Champion,
-    ChampionDetail,
-    Trailer,
-    Credit
-} from '../components/home-section'
+  Welcome,
+  Champion,
+  ChampionDetail,
+  Trailer,
+  Credit,
+} from "../components/home-section";
 
-import { championsData } from '../assets/dummy'
+import { championsData } from "../assets/dummy";
 
-SwiperCore.use([Mousewheel, Pagination, EffectFade])
+SwiperCore.use([Mousewheel, Pagination, EffectFade]);
 
 const swiperOptions = {
-    direction: 'vertical',
-    slidesPerView: 1,
-    spaceBetween: 0,
-    mousewheel: true,
-    pagination: true,
-    effect: 'fade',
-    speed: 1000
-}
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  mousewheel: true,
+  pagination: true,
+  effect: "fade",
+  speed: 1000,
+};
 
 const Home = () => {
-    return (
-        <>
-            <Swiper {...swiperOptions}>
-                <SwiperSlide>
-                    {({ isActive }) => <Welcome isActive={isActive}/>}
-                </SwiperSlide>
-                <SwiperSlide>
+  return (
+    <>
+      <Swiper {...swiperOptions}>
+        <SwiperSlide>
+          {({ isActive }) => <Welcome isActive={isActive} />}
+        </SwiperSlide>
+        {/*                 <SwiperSlide>
                     {({ isActive }) => <Champion isActive={isActive}/>}
                 </SwiperSlide>
                 <SwiperSlide>
@@ -46,20 +39,16 @@ const Home = () => {
                 </SwiperSlide>
                 <SwiperSlide>
                     {({ isActive }) => <Credit isActive={isActive}/>}
-                </SwiperSlide>
-            </Swiper>
-            {
-                championsData.map((item, index) => <ChampionDetail
-                    key={index}
-                    item={item}
-                    id={index}
-                />)
-            }
-            <div className="scroll">
-                <span>Scroll to see effect</span>
-            </div>
-        </>
-    )
-}
+                </SwiperSlide> */}
+      </Swiper>
+      {championsData.map((item, index) => (
+        <ChampionDetail key={index} item={item} id={index} />
+      ))}
+      {/* <div className="scroll">
+        <span>Scroll to see effect</span>
+      </div> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
