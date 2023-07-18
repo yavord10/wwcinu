23682d2d8c773ebf08d1e5d216f23dd57a614f7c
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-07-18 22:28:41 +0100

    More changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import './header.scss'
+import "./header.scss";
 
-import { logo } from '../../assets/images'
+import logo from "../../assets/images/worldcupinutransparent.png";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header__inner container">
-                <div className="header__logo">
-                    <img src={logo} alt="" />
-                </div>
-                <ul className="header__nav">
-                    <li><a href="#">overview</a></li>
-                    <li><a href="#">news</a></li>
-                    <li><a href="#">champions</a></li>
-                    <li><a href="#">support</a></li>
-                </ul>
-            </div>
+  return (
+    <div className="header">
+      <div className="header__inner container">
+        <div className="header__logo">
+          <img src={logo} alt="" />
         </div>
-    )
-}
+        <ul className="header__nav">
+          <li>
+            <a href="https://twitter.com/WWCUPINU">Twitter</a>
+          </li>
+          <li>
+            <a href="https://t.me/officialwomensworldcupinu">Telegram</a>
+          </li>
+          <li>
+            <a href="https://etherscan.io/address/0xef9B378Aa82787331f879D3D421974F1792E6866">
+              Etherscan
+            </a>
+          </li>
+          <li>
+            <a href="mailto:womensworldcupinu@gmail.com">Email</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
